62fb4eb866f9dbc95eb7a8316b16975985c3aaf7
Martin Skatvedt <martinskatvedt@gmail.com>
2023-10-04 14:52:12 +0200

    rewrite to pure sql

--- assignment2/queries.py
+++ assignment2/queries.py
@@ -10,69 +10,162 @@ class Queries:
         self.db_connection = self.connection.db_connection
         self.cursor = self.connection.cursor
 
-    def averageMinAndMaxTrackpoints(self):
-        count_dict = {} # user_id : n_trackpoints
-        table_name = "Activity"
-        query = "SELECT id, user_id FROM %s"
-        self.cursor.execute(query % table_name)
-        rows = self.cursor.fetchall()
-        for row in rows:
-            activity_id = row[0]
-            user_id = row[1]
-            query = "SELECT COUNT(*) FROM Trackpoint WHERE activity_id = '%s'"
-            self.cursor.execute(query % activity_id)
-            count = self.cursor.fetchall()
-            prev_count = count_dict.get(user_id, 0)
-            count_dict.update({user_id: prev_count + count[0][0]})
 
-        min = ["000", float("inf")]
-        max = ["000", float("-inf")]
-        total = 0.0
-        for user, count in count_dict.items():
-            total += count
-            if count < min[1]:
-                min = [user, count]
-            if count > max[1]:
-                max = [user, count]
-        average = total / len(count_dict)
+    def task_1(self):
+        """
+        How many users, activities and trackpoints are there in the 
+        dataset (after it is inserted into the database).  
+        """
 
-        print("Max number of trackpoints: " + str(max[1]) + " for user: " + max[0])
-        print("Min number of trackpoints: " + str(min[1]) + " for user: " + min[0])
-        print(f'Average over {len(count_dict)} users is: {average}')
+        query = """
+        WITH UserCount AS (
+            SELECT COUNT(*) AS user_count FROM User
+        ),
+        ActivityCount AS (
+            SELECT COUNT(*) AS activity_count FROM Activity
+        ),
+        TrackpointCount AS (
+            SELECT COUNT(*) AS trackpoint_count FROM Trackpoint
+        )
+        SELECT
+            user_count,
+            activity_count,
+            trackpoint_count
+        FROM UserCount, ActivityCount, TrackpointCount
 
-    def top15Users(self):
-        activity_dict = {} # user_id : n_activities
-        table_name = "Activity"
-        query = "SELECT user_id FROM %s"
-        self.cursor.execute(query % table_name)
-        rows = self.cursor.fetchall()
-        for row in rows:
-            user_id = row[0]
-            prev_activity = activity_dict.get(user_id, 0)
-            activity_dict.update({user_id: prev_activity + 1})
-            
-        sorted_dict = sorted(activity_dict.items(), key=lambda item: item[1], reverse=True)
-        print(sorted_dict[0: 15])
+        """
+
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+
+        print(tabulate(result, headers=["User count", "Activity count", "Trackpoint count"]))
+
+    def task_2(self):
+        """
+        Find the average, maximum and minimum number of trackpoints per user.
+        """
+        query = """
+        WITH TrackpointsPerUser AS (
+            SELECT 
+                user_id,
+                Activity.id,
+                COUNT(*) as n_trackpoints
+            FROM Activity
+            JOIN Trackpoint ON Activity.id = Trackpoint.activity_id
+            GROUP BY user_id, id
+        ),
+        RankTrackpointsPerUserDesc AS (
+            SELECT 
+                user_id, 
+                n_trackpoints,
+                RANK() OVER (PARTITION BY user_id ORDER BY n_trackpoints DESC) AS TP_rank
+            FROM TrackpointsPerUser
+        ),
+        MaxTrackpointsPerUser AS (
+            SELECT 
+                user_id, 
+                n_trackpoints 
+            FROM RankTrackpointsPerUserDesc
+            WHERE TP_rank = 1
+        ),
+        RankTrackpointsPerUserAsc AS (
+            SELECT
+                user_id,
+                n_trackpoints,
+                RANK() OVER (PARTITION BY user_id ORDER BY n_trackpoints ASC) AS TP_rank
+            FROM TrackpointsPerUser
+            ORDER BY user_id
+        ),
+        MinTrackpointsPerUser AS (
+            SELECT
+                user_id,
+                n_trackpoints
+            FROM RankTrackpointsPerUserAsc
+            WHERE TP_rank = 1
+        ),
+        AverageTrackpointsPerUser AS (
+            SELECT
+                user_id,
+                AVG (n_trackpoints) OVER (PARTITION BY user_id) AS avg_trackpoints
+            FROM TrackpointsPerUser
+        )
+
+        SELECT
+            DISTINCT min_tp.user_id, min_tp.n_trackpoints, max_tp.n_trackpoints, avg_tp.avg_trackpoints
+        FROM MinTrackpointsPerUser min_tp
+        JOIN MaxTrackpointsPerUser max_tp ON min_tp.user_id = max_tp.user_id
+        JOIN AverageTrackpointsPerUser avg_tp ON min_tp.user_id = avg_tp.user_id
+
+        """
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+
+        print(tabulate(result[0:15], headers=["User ID", "Min trackpoints", "Max trackpoints", "Average trackpoints"]))
+  
+    def task_3(self):
+        """
+        Find the top 15 users with the highest number of activities. 
+        """ 
+
+        query = """
+        SELECT 
+            user_id,
+            COUNT(*) AS activity_count
+        FROM Activity
+        GROUP BY user_id
+        ORDER BY activity_count DESC
+        LIMIT 15
+        """
+
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(tabulate(result, headers=["User ID", "Activity count"]))
+
+    def task_4(self):
+        """
+        Find all users who have taken a bus. 
+        """
+
+        query = """
+        SELECT 
+            DISTINCT user_id
+        FROM Activity
+        WHERE transportation_mode = 'bus'
+        """
+
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(tabulate(result, headers=["User ID"]))
+
+    def task_5(self):
+        """
+        List the top 10 users by their amount of different transportation modes.
+        """
         
-    def top10UsersWithUniqueActivities(self):
-        activity_dict = defaultdict(set) # user_id : transportation_mode[]
-        table_name = "Activity"
-        query = "SELECT user_id, transportation_mode FROM %s"
-        self.cursor.execute(query % table_name)
-        rows = self.cursor.fetchall()
-        for row in rows:
-            if row[1]:
-                user_id = row[0]
-                transportation_mode = row[1]
-                activity_dict[user_id].add(transportation_mode)
-        activity_counts = []
-        for key, value in activity_dict.items():
-            activity_counts.append([key, len(value)])
+        query = """
+        WITH TransportationModes AS (
+            SELECT 
+                user_id, 
+                transportation_mode
+            FROM Activity
+            WHERE transportation_mode IS NOT NULL
+            GROUP BY user_id, transportation_mode
+        )
+        SELECT
+            user_id,
+            COUNT(*) AS n_transportation_modes
+        FROM TransportationModes
+        GROUP BY user_id
+        ORDER BY n_transportation_modes DESC
+        LIMIT 10
+        """
 
-        sorted_counts = sorted(activity_counts, key=lambda item: item[1], reverse=True)
-        print(sorted_counts[0: 10])
-            
-    def multipleActivities(self):
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+
+        print(tabulate(result, headers=["User ID", "Number of transportation modes"]))
+
+    def task_6(self):
         table_name = "Activity"
         query = "SELECT user_id, start_date_time, end_date_time FROM %s"
         self.cursor.execute(query % table_name)
@@ -87,7 +180,7 @@ class Queries:
             if count[0][0] > 1:
                 print("User: " + user_id + " has multiple activities with the same start and end date time")
         
-    def nUsersThatHasStartedAnActivityAndEndedItTheNextDay(self):
+    def task_7(self):
         #This answers both a) and b)
         table_name = "Activity"
         query = "SELECT * FROM %s"
@@ -146,7 +239,7 @@ class Queries:
         distance = (c * r) * 1000 # Return distance in meters
         return distance
         
-    def nUsersWhichHaveBeenClose(self):
+    def task_8(self):
         overlapping_activities = self.overlappingActivities()
 
         has_been_close = defaultdict(set)
@@ -191,7 +284,7 @@ class Queries:
         
         print(f'Number of unique users that have been close to each other: {len(unique_users)}')
 
-    def gainedAltitude(self):
+    def task_9(self):
         altitudes_dict = {} # user_id : altitude
 
         #Fetches all users
@@ -234,11 +327,23 @@ class Queries:
         sorted_dict = sorted(altitudes_dict.items(), key=lambda item: item[1], reverse=True)
         print(tabulate(sorted_dict[0:15], headers=["User ID", "Altitude gained"]))
 
-    """
-    Find all users who have registered transportation_mode 
-    and their most used transportation_mode.
-    """
+ 
+    def task_11(self):
+        """
+        Find all users who have invalid activities, 
+        and the number of invalid activities per user.
+        """
+        
+        query = """
+
+        """
+
     def task_12(self):
+        """
+        Find all users who have registered transportation_mode 
+        and their most used transportation_mode.
+        """
+
         query = """
         WITH TM_COUNTS AS (
             SELECT 
@@ -273,7 +378,7 @@ def main():
     program = None
     try:
        program = Queries()
-       program.task_12()
+       program.task_5()
     
     except Exception as e:
         print("ERROR: Failed to use database:", e)
