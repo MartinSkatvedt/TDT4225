bbf566e7f57cd5a2b67d5381d20ef918f07d75cb
Martinskatvedt <martinskatvedt@gmail.com>
2023-10-13 10:18:43 +0200

    added queries class

---
+++ assignment3/queries.py
@@ -0,0 +1,31 @@
+from pprint import pprint 
+from DbConnector import DbConnector
+
+class Queries:
+
+    def __init__(self):
+        self.connection = DbConnector()
+        self.client = self.connection.client
+        self.db = self.connection.db
+
+    def fetch_documents(self, collection_name):
+        collection = self.db[collection_name]
+        documents = collection.find({})
+        for doc in documents: 
+            pprint(doc)
+
+
+def main():
+    program = None
+    try:
+        program = Queries()
+  
+    except Exception as e:
+        print("ERROR: Failed to use database:", e)
+    finally:
+        if program:
+            program.connection.close_connection()
+
+
+if __name__ == '__main__':
+    main()
